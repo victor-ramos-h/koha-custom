1d75adc5438ef69da9890b17c057331f69ceb516
carlos-fernandez-tierconnect-com <carlos.fernandez@mojix.com>
2020-02-27 21:26:53 -0400

    koha custom with external db configuration

---
+++ setup-koha.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+#docker load < koha-adp-image.tar.gz
+docker-compose up -d db
+echo "Configuring Database..."
+sleep 2m
+docker-compose exec -T db mysql -u root -pmBadp2019! -e "CREATE DATABASE koha_adp;"
+docker-compose exec -T db mysql -u root -pmBadp2019! mysql < mysql.sql
+docker-compose exec -T db mysql -u root -pmBadp2019! koha_adp < koha_adp.sql
+docker-compose exec -T db mysql -u root -pmBadp2019! -e "FLUSH PRIVILEGES;"
+docker-compose up -d koha
--- start-koha.sh
+++ start-koha.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-docker load < koha-adp-image.tar.gz
 docker-compose up -d koha
-docker-compose exec koha /kohadevbox/start.sh
